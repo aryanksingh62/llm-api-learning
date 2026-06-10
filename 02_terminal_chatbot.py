e45ccabf84826f1bed5b88f9bdf96f53d69b6c45
Aryan Singh <aryan98182@gmail.com>
2026-06-11 00:57:20 +0530

    update the terminal chatbot

--- 02_terminal_chatbot.py
+++ 02_terminal_chatbot.py
@@ -3,23 +3,33 @@ from dotenv import load_dotenv
 
 load_dotenv()
 client=OpenAI()
+
+messages=[{"role": "system", "content": "You are a helpful assistant."}]
+
 while True:
-    user_input=input("what you wanna talk (type exit to quit): ").strip()
+    user_input=input("what you wanna talk (type 'exit' to quit): ").strip()
 
     if user_input.lower()=="exit":
         print("Exiting chatbot...")
         break
-    if not user_input:
-        continue
 
-    response= client.responses.create(
+    messages.append({"role":"user","content":user_input})
+
+    with client.responses.stream(
         model="gpt-5.4-mini",
-        input= user_input,
-        stream=True
-    )
+        input= messages,
+    ) as response:
 
-    print("*"*30)
-    for event in response:
-        if event.type == "response.output_text.delta":
-            print(event.delta,end="",flush=True)
-    print()
+        full_response=""
+        print("*"*30)
+        print("Assistant: ", end="", flush=True)
+
+        for event in response:
+            if event.type == "response.output_text.delta":
+                print(event.delta,end="",flush=True)
+                full_response+=event.delta
+        print()
+        final_response= response.get_final_response()
+
+        messages.append({"role": "assistant", "content": full_response})
+        print(f"total token usage: {final_response.usage.total_tokens}")
